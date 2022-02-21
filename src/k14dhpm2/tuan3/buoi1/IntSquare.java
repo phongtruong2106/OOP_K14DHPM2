ce5e04f4900f3cedc3607f4e1c2c20b66df268c9
lhphuoc2311 <phuoc2311@gmail.com>
2022-02-21 09:47:43 +0700

    Int Square Ex Encapsulation

---
+++ src/k14dhpm2/tuan3/buoi1/IntSquare.java
@@ -0,0 +1,25 @@
+/*
+*  created date: Feb 21, 2022
+*  author: cgm
+*/
+package k14dhpm2.tuan3.buoi1;
+//Host
+public class IntSquare {
+
+    
+    //implementation
+   private  int squareValue;
+
+
+    //interfaces
+    int getSquareOperator(int value){/** toan tu X */
+
+        squareValue = value * value;
+
+        return squareValue;
+
+    }
+
+
+    
+}
---
+++ src/k14dhpm2/tuan3/buoi1/IntSquareTestDrive.java
@@ -0,0 +1,18 @@
+/*
+*  created date: Feb 21, 2022
+*  author: cgm
+*/
+package k14dhpm2.tuan3.buoi1;
+
+public class IntSquareTestDrive {
+    public static void main(String[] args) {
+
+        IntSquare intSquare = new IntSquare();
+
+        int result = intSquare.getSquareOperator(2);
+
+        System.out.println("Result: " + result);
+        
+    }
+    
+}
